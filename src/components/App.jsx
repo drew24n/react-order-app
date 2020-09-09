44ad1b6dc62e686b73e6f84cc4647dd2f4c4dd2d
Andrii <drew24n@gmail.com>
2020-09-09 14:58:16 +0300

    logic fixes and improvements, styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,19 @@ export const App = () => {
     const dispatch = useDispatch()
     const appState = useSelector(state => state.app)
 
-    const languages = ['Английский', 'Русский', 'Украинский']
+    const languages = ['Украинский', 'Русский', 'Английский']
 
     //price calculations
+    const [fileTypePriceRate, setFileTypePriceRate] = useState(0)
+    const [fileContentLength, setFileContentLength] = useState(0)
+    const [price, setPrice] = useState(0)
+
     let languagePriceRate = appState.currentLang === 'Английский' ? 0.12 : 0.05
     let minPrice = appState.currentLang === 'Английский' ? 120 : 50
 
-    const [fileTypePriceRate, setFileTypePriceRate] = useState(0)
-    const [fileContentLength, setFileContentLength] = useState(0)
+    let contentLength = fileContentLength > 0 ? fileContentLength : appState.text.length
+    let basicPrice = languagePriceRate * contentLength
+    let finalPrice = basicPrice * fileTypePriceRate + basicPrice
 
     const processFileData = (file) => {
         if (file.target.files[0]) {
@@ -32,31 +37,11 @@ export const App = () => {
         }
     }
 
-    let contentLength = fileContentLength > 0 ? fileContentLength : appState.text.length
-    let basicPrice = languagePriceRate * contentLength
-    let finalPrice = basicPrice * fileTypePriceRate + basicPrice
+    //deadline and job time calculations
+    const [time, setTime] = useState(0)
 
-    const [price, setPrice] = useState(0)
-
-    useEffect(() => {
-        if (!contentLength || !appState.currentLang) return setPrice(0)
-        if (finalPrice < minPrice) setPrice(minPrice)
-        else setPrice(finalPrice)
-    }, [appState.currentLang, contentLength, fileTypePriceRate])
-    //price calculations end
-
-    //deadline and time calculations
     const minTime = 3600
     let currentSpeedRate = appState.currentLang === 'Английский' ? 0.093 : 0.37 //typing speed, symbols per second
-
-    const estimateJobTime = () => {
-        let basicTime = minTime + contentLength / currentSpeedRate
-        if (!contentLength || !appState.currentLang) return 0
-        return basicTime * fileTypePriceRate + basicTime //time depends on file type - fileTypePriceRate
-    }
-
-    const [time, setTime] = useState(0)
-    useEffect(() => setTime(estimateJobTime()), [contentLength, currentSpeedRate, fileTypePriceRate])
 
     let h = time / 3600 ^ 0
     let m = (time - h * 3600) / 60 ^ 0
@@ -82,30 +67,62 @@ export const App = () => {
     //
     // calcDeadline()
 
-    return <div className={style.container}>
-        <form>
-            {/*<input value={appState.email} onChange={e => dispatch(setEmail(e.target.value))} type="email"*/}
-            {/*       placeholder={'email'}/>*/}
-            {/*<input value={appState.name} onChange={e => dispatch(setName(e.target.value))} type="text"*/}
-            {/*       placeholder={'name'}/>*/}
+    useEffect(() => {
+        if (!contentLength || !appState.currentLang) {
+            setTime(0)
+            setPrice(0)
+        } else {
+            let basicTime = minTime + contentLength / currentSpeedRate
+            let finalJobTime = basicTime * fileTypePriceRate + basicTime //time depends on file type - fileTypePriceRate
+            setTime(finalJobTime)
+
+            if (finalPrice < minPrice) {
+                setPrice(minPrice)
+            } else setPrice(finalPrice)
+        }
+    }, [contentLength, fileTypePriceRate, appState.currentLang, currentSpeedRate, finalPrice, minPrice])
+
+    return (
+        <form className={style.container}>
             <div>
-                <textarea value={appState.text} onChange={e => dispatch(setText(e.target.value))} placeholder={'text'}/>
-                <input type="file" onChange={e => processFileData(e)}/>
-            </div>
-            <div>{fileContentLength ? fileContentLength : appState.text.length}</div>
-            <div onChange={e => dispatch(setLang(e.target.value))}>
-                {languages.map((l, index) => (
-                    <div key={index}>
-                        <label htmlFor={l}>{l}</label>
-                        <input type="radio" name={'lang'} value={l} id={l}/>
+                <div className={style.firstSection}>
+                    <h3>ЗАКАЗАТЬ РЕДАКТИРОВАНИЕ</h3>
+                    <p>Исправим все ошибки и огрехи, уберем все глупости из текста, перефразируем неудачные места, но
+                        сильно переписывать текст не станем. Лишних правок не будет.
+                        <a href="/#"> Подробнее о редактировании</a>
+                    </p>
+                    <input value={appState.email} onChange={e => dispatch(setEmail(e.target.value))} type="email"
+                           placeholder={'Ваша эл. почта'}/>
+                    <input value={appState.name} onChange={e => dispatch(setName(e.target.value))} type="text"
+                           placeholder={'Ваше имя'}/>
+                    <div>
+                        <textarea value={appState.text} onChange={e => dispatch(setText(e.target.value))}
+                                  placeholder={'Введите текст или загрузите файл'}/>
+                        <label htmlFor="image_uploads">загрузите файл
+                            <input id="image_uploads" type="file" onChange={e => processFileData(e)}/>
+                        </label>
+                        <div>{contentLength}</div>
                     </div>
-                ))}
+                </div>
+                <div onChange={e => dispatch(setLang(e.target.value))}>
+                    <h3>Язык</h3>
+                    {languages.map((l, index) => (
+                        <div key={index}>
+                            <input type="radio" name={'lang'} value={l} id={l}/>
+                            <label htmlFor={l}>{l}</label>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    <input value={appState.comment} onChange={e => dispatch(setComment(e.target.value))} type="text"
+                           placeholder={'Короткий комментарий или ссылка'}/>
+                </div>
             </div>
-            {/*<input value={appState.comment} onChange={e => dispatch(setComment(e.target.value))} type="text"*/}
-            {/*       placeholder={'comment'}/>*/}
-            {/*<button>Заказать</button>*/}
+            <div>
+                <div>{price.toFixed(2)} грн.</div>
+                <div>Сдадим через: {h} ч. {m} мин. {s} сек.</div>
+                <button>Заказать</button>
+            </div>
         </form>
-        <div>Цена: {price.toFixed(2)}</div>
-        <div>Время: {h} час {m} минут {s} секунд</div>
-    </div>
+    )
 }
